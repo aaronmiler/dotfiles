80372b3464dce73b77e0e2ea4611e3e67c737753
Aaron Miler <amiler@wsgc.com>
2013-11-22 11:54:50 -0800

    Added vimrc

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc pryrc"    # list of files/folders to symlink in homedir
+files="zshrc pryrc vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
